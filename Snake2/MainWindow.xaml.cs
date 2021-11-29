fdee6c77e5bb3e2d6d7ccc4c890de53485106c28
unknown <carlosaarman@gmail.com>
2021-11-29 16:46:10 +0100

    Versio 3, la serp es mou...

--- Snake2/MainWindow.xaml.cs
+++ Snake2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Snake2
 {
@@ -20,9 +21,36 @@ namespace Snake2
     /// </summary>
     public partial class MainWindow : Window
     {
+        SnakeGame jocSerp = new SnakeGame();
+        DispatcherTimer timer = new DispatcherTimer();
+
         public MainWindow()
         {
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
             InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Redibuixar cada vegada.
+            int tamanyXCasella = (int)canvas.ActualWidth / SnakeGame.X_SIZE;
+            int tamanyYCasella = (int)canvas.ActualHeight / SnakeGame.Y_SIZE;
+            Ellipse ellSerp = new Ellipse()
+            {
+                Fill = Brushes.Green,
+                Width = canvas.ActualWidth / SnakeGame.X_SIZE,
+                Height = canvas.ActualHeight / SnakeGame.Y_SIZE
+            };
+            canvas.Children.Add(ellSerp);
+            Canvas.SetTop(ellSerp, jocSerp.CapSerp.Y * tamanyYCasella);
+            Canvas.SetLeft(ellSerp, jocSerp.CapSerp.X * tamanyXCasella);
+        }
+
+        private void canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            jocSerp.Moure();
         }
     }
 }
---
+++ Snake2/SnakeGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake2
+{
+    public enum DireccioSnake
+    {
+        Dreta,
+        Esquerra,
+        Avall,
+        Amunt
+    }
+    class SnakeGame
+    {
+
+        public const int X_SIZE = 5;
+        public const int Y_SIZE = 5;
+
+        Point capSerp = new Point(0,0);
+        DireccioSnake direccio = DireccioSnake.Avall;
+
+        public Point CapSerp { get => capSerp; set => capSerp = value; }
+        public DireccioSnake Direccio { get => direccio; set => direccio = value; }
+
+        internal void Moure()
+        {
+            capSerp.Y++;
+        }
+    }
+
+}
